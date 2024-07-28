565d88f31f09eff6f4e61aab7af552933bf22574
eduardocaizaluisa <eduardo.caizaluisa@unl.edu.ec>
2024-07-28 09:37:48 -0500

    Update Imprimir_ pares_matriz.c

--- Imprimir_ pares_matriz.c
+++ Imprimir_ pares_matriz.c
@@ -15,6 +15,17 @@ void imprimirPares(int matriz[FILAS][COLUMNAS]) {
     printf("\n");
 }
 
+void imprimirMatriz(int matriz[FILAS][COLUMNAS]) {
+    printf("Matriz: \n");
+    for (int i = 0; i < FILAS; i++) {
+        for (int j = 0; j < COLUMNAS; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
 int main() {
     int matriz[FILAS][COLUMNAS] = {
         {1, 2, 3, 4},
@@ -23,6 +34,8 @@ int main() {
         {13, 14, 15, 16}
     };
 
+    imprimirMatriz(matriz);
     imprimirPares(matriz);
     return 0;
 }
+
